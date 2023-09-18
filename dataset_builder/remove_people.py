8c2ebec8fe50d1ca6f908e5703eef689326cf4c2
Ernesto Diaz <zaidotsenre@gmail.com>
2023-09-18 03:29:17 -0400

    Implemented remove_people
    - Wrote the main funtion for remove people

--- dataset_builder/remove_people.py
+++ dataset_builder/remove_people.py
@@ -1,19 +1,24 @@
 import boto3
+import os
+import re
 
 
-def remove_with_people(img_path, min_confidence=50):
-    if os.path.exists(img_path):
-        print(f'Looking for people in: {img_path}')
-        rekognition = boto3.client("rekognition")
-        img_bytes = get_image_from_file(img_path)
-        try:
-            response = rekognition.detect_labels(Image={'Bytes': img_bytes}, MinConfidence=min_confidence,
-                                             Settings={"GeneralLabels": {"LabelInclusionFilters": ["Person"]}})
-        except Exception as e:
-            return False
-        for label in response["Labels"]:
-            if label["Name"] == "Person":
-                os.remove(img_path)
-                return True
+def get_image_from_file(path):
+    with open(path, 'rb') as img_file:
+        return img_file.read()
 
-    return False
+
+def main():
+    for path in os.listdir():
+        if re.search('\.(jpg|png|jpeg)\Z', path) is not None:
+            img_bytes = get_image_from_file(path)
+            rekognition = boto3.client("rekognition")
+            response = rekognition.detect_labels(Image={'Bytes': img_bytes}, MinConfidence=90,
+                                                 Settings={"GeneralLabels": {"LabelInclusionFilters": ["Person"]}})
+            for label in response["Labels"]:
+                if label["Name"] == "Person":
+                    os.remove(path)
+
+
+if __name__ == '__main__':
+    main()
