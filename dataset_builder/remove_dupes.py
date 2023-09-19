e3e3f76e0723f3e19a05f223dcdc6f55bc123418
Ernesto Diaz <zaidotsenre@gmail.com>
2023-09-18 23:11:05 -0400

    Implemented remove dupes
    - Implemented remove dupes using the imagehash library.

--- dataset_builder/remove_dupes.py
+++ dataset_builder/remove_dupes.py
@@ -1,15 +1,38 @@
 import os
 import hashlib
+import imagehash
+from PIL import Image
+
+
+def main2():
+    img_hashes = set()
+    for path in os.listdir():
+        f = open(path, 'rb')
+        img = f.read()
+        f.close()
+        img_hash = hashlib.md5(img).hexdigest()
+        if img_hash in img_hashes:
+            os.remove(path)
+        else:
+            img_hashes.add(img_hash)
+
+
+def has_near_duplicate(img_hashes, img_hash, tolerance):
+    for h in img_hashes:
+        if abs(h - img_hash) <= tolerance:
+            return True
 
 
 def main():
+    img_hashes = set()
     for path in os.listdir():
-        img_hashes = set()
+        img = Image.open(path).convert('RGBA')
+        img_hash = imagehash.average_hash(img)
+        if has_near_duplicate(img_hashes, img_hash, 10):
+            os.remove(path)
+        else:
+            img_hashes.add(img_hash)
 
-        with open(path, "rb") as f:
-            img = f.read()
-            img_hash = hashlib.md5(img).hexdigest()
-            if img_hash in img_hashes:
-                os.remove(path)
-            else:
-                img_hashes.add(img_hash)
+
+if __name__ == '__main__':
+    main()
