293835821301bf97244734c25dd177aaf7b3717f
Ernesto Diaz <zaidotsenre@gmail.com>
2023-09-19 20:54:35 -0400

    Custom Label Detection
    Wrote Function in http_server to detect labels in images using custom rekognition model

--- cv_service/http_client.py
+++ cv_service/http_client.py
@@ -2,7 +2,7 @@ import requests
 import json
 import base64
 
-with open('input.png', 'rb') as f:
+with open('tst.jpeg', 'rb') as f:
     img = f.read()
     #r = requests.post("http://54.89.243.235:5000/label", data=img)
     r = requests.post("http://127.0.0.1:5000/label", data=img)
--- cv_service/http_server.py
+++ cv_service/http_server.py
@@ -3,19 +3,24 @@ import boto3
 
 app = Flask(__name__)
 
-def get_labels(img):
+
+def get_label(img):
+    model = ('arn:aws:rekognition:us-east-1:422292458603:project/gym_equipment/version/gym_equipment.2023-09-19T00.49'
+             '.22/1695098962266')
     rekognition = boto3.client("rekognition")
-    response = rekognition.detect_labels(Image={'Bytes': img},
-                                         Settings={"GeneralLabels": {"LabelInclusionFilters": ["Person"]}})
-    for label in response["Labels"]:
-        if label["Name"] == "Person":
-            os.remove(path)
+    response = rekognition.detect_custom_labels(Image={'Bytes': img},
+                                                MinConfidence=50,
+                                                ProjectVersionArn=model)
+    if len(response['CustomLabels']) > 0:
+        return response['CustomLabels'][0]['Name']
+    else:
+        return 'No Match'
+
 
 @app.route('/label', methods=['POST'])
 def label():
-    with open('output.png', 'wb') as img:
-        img.write(request.data)
-    return 'Received !'  # response to your request.
+    return get_label(request.data)
 
 
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
